e14f78a201b7febf40aea34a8632507771f8d040
ShinjungOh <tlswjd9260@gmail.com>
2023-02-22 12:16:26 +0900

    design: emotion 적용

---
+++ freeboard/pages/emotion/index.js
@@ -0,0 +1,16 @@
+import { Email } from '../../styles/emotion';
+
+const EmotionPage = () => {
+    //  js
+
+    return (
+        <>
+            <Email>이메일</Email>
+            <input type="text"/>
+            <button>Click!</button>
+            <img src="/vercel.svg" alt=""/>
+        </>
+    );
+};
+
+export default EmotionPage;
