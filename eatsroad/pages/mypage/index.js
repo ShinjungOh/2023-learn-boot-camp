2a4747ecc7824da9e6d7cfd44d6d43d38f3a11bb
ShinjungOh <tlswjd9260@gmail.com>
2023-02-23 04:48:57 +0900

    chore: project init

---
+++ eatsroad/pages/mypage/index.js
@@ -0,0 +1,17 @@
+import { styled } from '@emotion/styled';
+
+const MyPage = () => {
+    // js
+
+    return (
+        <Container>
+            <div>mypage</div>
+        </Container>
+    );
+}
+
+export default MyPage;
+
+const Container = styled.div`
+    
+`;
